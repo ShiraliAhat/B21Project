d20d48459e6237f37e064c873125cb286fddebd0
Shirali-Ahat <shirali0215@gmail.com>
2020-11-10 09:49:29 -0800

    project is already in remote repo

--- src/hello.java
+++ src/hello.java
@@ -2,4 +2,11 @@ public class hello {
 
     //more stuff
     //Mac user and windows user are happy to see commit dialog finally
+
+    //I have already published on GitHub.com
+    //now I am ready to make commits
+
+
+
+
 }
